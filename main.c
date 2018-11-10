15e610714645e479e4d0bfb9fd2a80b24d925199
Gerardo <gerh2712@outlook.com>
2018-11-09 22:49:50 -0600

    maquetado del programa

---
+++ main.c
@@ -0,0 +1,43 @@
+/*
+Programa 1
+
+Análisis
+    Entradas
+    Salidas
+    Restricciones
+*/
+/*
+Programa 2
+
+Análisis
+    Entradas
+    Salidas
+    Restricciones
+*/
+/*
+Programa 3
+
+Análisis
+    Entradas
+    Salidas
+    Restricciones
+*/
+
+//Includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Prototipo de funciones
+
+
+//Función principal
+int main()
+{
+    
+    
+    return 0;
+}
+
+
+//Funciones
